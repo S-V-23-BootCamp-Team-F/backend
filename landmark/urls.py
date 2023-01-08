a6f0f6622b60438259c47b087a7a4275bf43c963
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-09 03:02:06 +0900

    feat: email id가져오기

--- landmark/urls.py
+++ landmark/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path("pictures/", views.s3Upload, name="s3Upload"),
+    path("histories/", view=views.gethistories, name="hisories"),
 ]
--- landmark/views.py
+++ landmark/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.decorators import api_view
 from django.utils.decorators import method_decorator
 from .storages import FileUpload, s3_client
-
+from .models import Member
+from rest_framework.response import Response
+from members.serializer import MemberSerializer
 # Create your views here.
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -17,3 +19,10 @@ def s3Upload(request) :
         "url" : profile_image_url
     }
     return JsonResponse(result, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def gethistories(request):
+    member = Member.objects.get(email=request.data['email'])
+    print(member.pk)
+    serializer = MemberSerializer(member)
+    return Response(serializer.data,status=status.HTTP_200_OK)
