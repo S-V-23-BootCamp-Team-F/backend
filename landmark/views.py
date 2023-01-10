2e404a2262b55c9c46bd5651f0b6738b38e999af
지윤 <dlwldbs369@tukorea.ac.kr>
2023-01-11 00:49:05 +0900

    fix:사진 업로드 API 수정
    - s3Upload함수 annotate 변경

--- landmark/views.py
+++ landmark/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from django.utils.decorators import method_decorator
 from .storages import FileUpload, s3_client
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
-
-@method_decorator(csrf_exempt, name='dispatch')
+@csrf_exempt
+@api_view(['POST'])
 def s3Upload(request) :
     file = request.FILES['picture']
-    email = request.POST['email']
     profile_image_url = FileUpload(s3_client).upload(file)
     result = {
         "message" : "사진 업로드 성공",
