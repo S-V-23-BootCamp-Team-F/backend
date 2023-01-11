487a7b3b23aeccdc7a1c431f667dff75bd83ed33
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-11 15:38:13 +0900

    fix:histories API 변경완료

--- members/models.py
+++ members/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django import forms
 
 # Create your models here.
 class Member(models.Model):
     email = models.EmailField(max_length=100, verbose_name="사용자 이메일")
+    password = models.CharField(verbose_name="사용자 비밀번호",max_length=100)
     user_status = models.BooleanField(default=True, verbose_name="사용자 상태")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="사용자 생성일")
     
--- plants/views.py
+++ plants/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .storages import FileUpload, s3_client
-from .models import Member,Plant
+from .models import Member,Plant,Disease
 from rest_framework.response import Response
 from .serializer import PlantSerializer
 # Create your views here.
@@ -22,6 +22,14 @@ def s3Upload(request) :
 @api_view(['GET'])
 def gethistories(request):
     member = Member.objects.get(email=request.data['email'])
-    histories = Plant.objects.filter(member_id = member.pk)
+    histories = Plant.objects.filter(member = member.pk).select_related('disease')
+    
+    # diseases = []
+    for history in histories:
+        print(history.disease.disease_code)
+        # disease = Disease.objects.get(id=history.disease_id)
+
+    # histories = Plant.objects.select_related('member_id')
     serializer = PlantSerializer(histories,many=True)
+    print(serializer.data)
     return Response(serializer.data,status=status.HTTP_200_OK)
