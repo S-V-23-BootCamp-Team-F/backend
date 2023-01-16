67b064cbbba0982730c69068c028fedd97e0e496
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-16 21:06:53 +0900

    feat: login & signup API 완료

--- members/serializer.py
+++ members/serializer.py
@@ -5,3 +5,9 @@ class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model=Member
         fields = "__all__"
+    
+    def create(self, validated_data):
+        member = Member.objects.create(email=validated_data['email'])
+        member.set_password(validated_data['password'])
+        member.save()
+        return member
