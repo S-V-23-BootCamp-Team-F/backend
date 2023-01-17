7065cfa341b52fa98d00eb1fbb7108fd03c4595b
지윤 <dlwldbs369@tukorea.ac.kr>
2023-01-18 04:58:13 +0900

    chore : 이미지 업로드에 필요한 Parser 추가

--- backend/settings.py
+++ backend/settings.py
@@ -33,9 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -214,7 +212,13 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
     )
+
 }
 
 SIMPLE_JWT = {
