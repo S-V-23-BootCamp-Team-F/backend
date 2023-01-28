797a8dcec4dc72f0dea6ce43fd8fd77865db72f3
지윤 <dlwldbs369@tukorea.ac.kr>
2023-01-29 00:32:36 +0900

    feat: barChart에 넘겨줄 데이터 뽑아오기
    - plant_id를 동일시 해야하는 작업 필요

--- plants/urls.py
+++ plants/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path("pictures/", views.s3Upload, name="s3Upload"),
     path("histories/", view=views.gethistories, name="hisories"),
     path("ais/",views.airequest,name="ais"),
-    path("histories/<int:diagnosis_id>/",view=views.deleteHistory,name="deletehistory")
+    path("histories/<int:diagnosis_id>/",view=views.deleteHistory,name="deletehistory"),
+    path("barchart/",view=views.barChart,name="barchart")
 ]
